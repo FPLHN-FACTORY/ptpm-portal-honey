a188e22d38489a894ed35f2fcc767e2e4dcd1839
hungpvph25929 <hungpvph25929@fpt.edu.vn>
2023-03-12 14:44:27 +0700

    no message

--- front_end/src/route.js
+++ front_end/src/route.js
@@ -3,7 +3,13 @@ app.config(function ($routeProvider, $locationProvider) {
   $locationProvider.hashPrefix("");
   $routeProvider
     .when("/home", {
-      templateUrl: "pages/home.html",
+      templateUrl: "pages/pages-student/home.html",
+    })
+    .when("/nhiem-vu", {
+      templateUrl: "pages/pages-student/mission.html",
+    })
+    .when("/qua-tang", {
+      templateUrl: "pages/pages-student/gift.html",
     })
     .otherwise({
       redirectTo: "/home",
